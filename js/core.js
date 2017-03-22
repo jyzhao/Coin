d451be4f1b7663c8faec48c5fb4b6b0afe404774
Jiyuan Zhao <jy.zhao@outlook.com>
2017-03-22 16:20:03 -0700

    Minor functional updates and small tweaking in UI
    Added ng-material for prompt dialog
    Removed redundant declaration and file import in css and html

--- js/core.js
+++ js/core.js
@@ -1,11 +1,7 @@
 /**
  * Created by Jiyuan on 2017/3/9.
  *
- * The core of this web app is a famous DP (dynamic programming) problem
- * I've spent 2 hours during the lunch break today to implement the bulk
- * of this program.
- * There are still small things to work on such as mobile-first and responsive design, documentation, etc.
- * I will update the program after work and commit to GitHub daily.
+ * The core of this web app is a famous DP (dynamic programming) problem.
  * If you have any questions during this development period,
  * please email me at zhao.jiyu@husky.neu.edu
  */
@@ -14,33 +10,49 @@
  so that the performance is not limited by the browser's javascript engine.
  Another way is to add a hash to the algorithm so that it's not necessary to calculate
  the same data repeatedly. Therefore, we can reduce some time and space complexity.
+
+ UPDATE: you can also use Web Worker to take over the computational expensive task
  */
 
-angular.module('myApp', []).controller('myController', function ($scope) {
+angular.module('myApp', ['ngMaterial']).controller('myController', function ($scope, $mdDialog) {
     $scope.coins = [25, 10, 5, 1];
     $scope.total = 0;
 
     enterCoin = function (coinNum) {
-        var coinValue = Number(prompt("Please enter a coin value."));
-        if (Number.isInteger(coinValue) &&              //check if it is integer
-            coinValue > 0 &&                            //check if coin value is positive
-            coinValue != 1 &&                           //make sure coin value is not 1
-            $scope.coins.indexOf(coinValue) == -1) {    //make sure there are no duplicates
-            $scope.coins[coinNum] = parseInt(coinValue);
-            $scope.$apply();                            //refresh data
-        }
-        else {
-            alert("Please enter a unique positive integer > 1");
-        }
+        var coinValue;
+
+        $scope.showPromptBox = function (ev) {
+            var confirm = $mdDialog.prompt()
+                .title('Enter A Coin Value?')
+                .textContent('a unique positive integer > 1')
+                .placeholder('Coin Value')
+                .ariaLabel('Coin Value')
+                .initialValue($scope.coins[ev])
+                .ok('Okay')
+                .cancel('Cancel');
+
+            $mdDialog.show(confirm).then(function (result) {
+                //console.log(result);
+                coinValue = Number(result);
+            });
+
+            if (Number.isInteger(coinValue) &&              //check if it is integer
+                coinValue > 0 &&                            //check if coin value is positive
+                coinValue !== 1 &&                           //make sure coin value is not 1
+                $scope.coins.indexOf(coinValue) === -1) {    //make sure there are no duplicates
+                $scope.coins[coinNum] = parseInt(coinValue);
+                $scope.$apply();                            //refresh data
+            }
+            else {
+                alert("Please enter a unique positive integer > 1");
+            }
+        };
     };
 
     $scope.calculate = function () {
         $scope.amounts = [0, 0, 0, 0];
-        //console.log($scope.total);
-        //console.log($scope.coins);
-        //console.log(change($scope.total,$scope.coins));
+
         var combination = change($scope.total, $scope.coins)[1];
-        //console.log(combination);
         for (var i = 0; i < combination.length; i++) switch (combination[i]) {
             case $scope.coins[0]:
                 $scope.amounts[0]++;
@@ -58,12 +70,13 @@ angular.module('myApp', []).controller('myController', function ($scope) {
     };
 
     function change(total, coins) {
-        if (total === 0) {
+        if (total === 0) {                      //base case for dp
             return [0, []];
         }
         if (coins.length === 0 && total > 0) {
             return [Infinity, []];
         }
+
         if (coins[0] > total) {
             return change(total, coins.slice(1));
         } else {
